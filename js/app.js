dbc908101f1105e1133b18a295b5bee5bce0c371
julfiker755 <julfiker755.bd@gmail.com>
2022-02-21 07:01:09 -0800

    four commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-// Example-1
-var btn=document.querySelector("#button11");
-
-var color=["red","green","orange","blue",'white',"palevioletred","mediumvioletred","orangered"]
-
-function colorchange(){
-    var num=color[Math.floor(Math.random()*color.length)];
-    document.body.style.backgroundColor=num;
-
+function red(){
+    document.body.style.backgroundColor="red";
+}
+function green(){
+    document.body.style.backgroundColor="green";
+}
+function orange(){
+    document.body.style.backgroundColor="orange";
+}
+function blue(){
+    document.body.style.backgroundColor="blue";
+}
+function white(){
+    document.body.style.backgroundColor="white";
 }
